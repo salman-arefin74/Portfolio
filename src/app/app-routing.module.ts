d79b2282b9cb65d5025572ab4131526eb03a3b74
Salman Arefin <arefinsalman86@gmail.com>
2021-01-19 23:20:03 +0600

    Skills section done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutSectionComponent } from './about-section/about-section.component';
+import { BlogSectionComponent } from './blog-section/blog-section.component';
+import { EducationSectionComponent } from './education-section/education-section.component';
+import { ExperienceSectionComponent } from './experience-section/experience-section.component';
+import { ProfessionalProjectsSectionComponent } from './professional-projects-section/professional-projects-section.component';
+import { SkillsSectionComponent } from './skills-section/skills-section.component';
 
-const routes: Routes = [];
+const routes: Routes = 
+[
+  {path: '', redirectTo: '/about', pathMatch: 'full'},
+  {path: 'about', component: AboutSectionComponent},
+  {path: 'experience', component: ExperienceSectionComponent},
+  {path: 'education', component: EducationSectionComponent},
+  {path: 'skills', component: SkillsSectionComponent},
+  {path: 'projects', component: ProfessionalProjectsSectionComponent},
+  {path: 'blog', component: BlogSectionComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
